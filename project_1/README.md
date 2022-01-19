19ccf31e3d1bf54ddc9e48a785cf70dd3447e3a8
Marwan Farrah <eng.marwan.dawood@gmail.com>
2022-01-19 22:57:33 +0000

    project_1: implement data analysis and splitting

--- project_1/README.md
+++ project_1/README.md
@@ -67,7 +67,7 @@ In the classroom workspace, every library and package should already be installe
 
 You should use the data already present in `/home/workspace/data/waymo` directory to explore the dataset! This is the most important task of any machine learning project. To do so, open the `Exploratory Data Analysis` notebook. In this notebook, your first task will be to implement a `display_instances` function to display images and annotations using `matplotlib`. This should be very similar to the function you created during the course. Once you are done, feel free to spend more time exploring the data and report your findings. Report anything relevant about the dataset in the writeup.
 
-Keep in mind that you should refer to this analysis to create the different spits (training, testing and validation).
+Keep in mind that you should refer to this analysis to create the different splits (training, testing and validation).
 
 
 ### Create the training - validation splits
--- project_1/create_splits.py
+++ project_1/create_splits.py
@@ -2,32 +2,74 @@ import argparse
 import glob
 import os
 import random
+import shutil
 
 import numpy as np
-
 from utils import get_module_logger
 
 
-def split(source, destination):
+def create_folder(path, directory):
+    if not os.path.exists(path + directory):
+        os.mkdir(directory)
+
+def delete_folder(path):
+    try:
+        shutil.rmtree(path)
+    except OSError as error:
+        print(error)
+        print(path + " can not be removed")
+
+
+def split_files(count, path, files):
+    for i in range(count):
+        p = files.pop()
+        os.symlink('preprocessed_data' + p, path + p)
+    print(path + " files has been splitted successfully!")
+
+
+def split(data_dir):
     """
     Create three splits from the processed records. The files should be moved to new folders in the
     same directory. This folder should be named train, val and test.
 
     args:
-        - source [str]: source data directory, contains the processed tf records
-        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+        - data_dir [str]: data directory, /home/workspace/data/preprocessed_data
     """
-    # TODO: Implement function
+    # TODO: Complete this function
+    os.chdir("data")
+    total = 0
+    files = []
+    src_path = data_dir + "/preprocessed_data"
+
+    for file in os.listdir(src_path):
+        if file.endswith(".tfrecord"):
+            total += 1
+            files.append(file)
+
+    random.shuffle(files)
+    train = int(75 / 100 * total)
+    val = int(15 / 100 * total)
+    test = int(10 / 100 * total)
+
+    delete_folder('train/')
+    delete_folder('test/')
+    delete_folder('val/')
+
+    create_folder(data_dir, 'train/')
+    create_folder(data_dir, 'test/')
+    create_folder(data_dir, 'val/')
+
+    split_files(train, 'train/', files)
+    split_files(test, 'test/', files)
+    split_files(val, 'val/', files)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser.add_argument('--data_dir', required=True,
+                        help='data directory')
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
     logger.info('Creating splits...')
-    split(args.source, args.destination)
+    split(args.data_dir)
