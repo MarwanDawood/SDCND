c8c00f59645b402cf8fb9e5aa4de8629a65a316d
Marwan Farrah <eng.marwan.dawood@gmail.com>
2022-01-22 22:27:37 +0000

    project_1: update split files

--- project_1/create_splits.py
+++ project_1/create_splits.py
@@ -18,18 +18,18 @@ def delete_folder(path):
     except OSError as error:
         print(error)
         print(path + " can not be removed")
-
-
+    
+    
 def split_files(count, path, files):
     for i in range(count):
         p = files.pop()
-        os.symlink('preprocessed_data' + p, path + p)
+        os.symlink('preprocessed_data/' + p, path + p)
     print(path + " files has been splitted successfully!")
 
-
+        
 def split(data_dir):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the
+    Create three splits from the processed records. The files should be moved to new folders in the 
     same directory. This folder should be named train, val and test.
 
     args:
@@ -39,8 +39,8 @@ def split(data_dir):
     os.chdir("data")
     total = 0
     files = []
-    src_path = data_dir + "/preprocessed_data"
-
+    src_path = data_dir + "/preprocessed_data/"
+    
     for file in os.listdir(src_path):
         if file.endswith(".tfrecord"):
             total += 1
@@ -50,21 +50,21 @@ def split(data_dir):
     train = int(75 / 100 * total)
     val = int(15 / 100 * total)
     test = int(10 / 100 * total)
-
+    
     delete_folder('train/')
     delete_folder('test/')
     delete_folder('val/')
-
+    
     create_folder(data_dir, 'train/')
     create_folder(data_dir, 'test/')
     create_folder(data_dir, 'val/')
-
+    
     split_files(train, 'train/', files)
     split_files(test, 'test/', files)
     split_files(val, 'val/', files)
+    
 
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
     parser.add_argument('--data_dir', required=True,
                         help='data directory')
